525be2900a6a36e9762f8e964a966703c58ba79a
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2024-11-23 16:00:29 -0300

    feat: add tests for 'isSunk()' method

--- src/ships/ships.test.js
+++ src/ships/ships.test.js
@@ -19,3 +19,27 @@ test("When invoked, 'hit()' method increases new ship 'hits' from '0' to '1'", (
     }),
   );
 });
+
+test("The number of hits is equal to the ship's length, therefore 'isSunk()' should return 'sunk' as 'true'", () => {
+  const shipC = new ships(1);
+  shipC.hit();
+  shipC.isSunk();
+
+  expect(shipC).toEqual(
+    expect.objectContaining({
+      sunk: true,
+    }),
+  );
+});
+
+test("The number of hits is not equal to the ship's length, therefore 'isSunk()' should return 'sunk' as 'false'", () => {
+  const shipD = new ships(2);
+  shipD.hit();
+  shipD.isSunk();
+
+  expect(shipD).toEqual(
+    expect.objectContaining({
+      sunk: false,
+    }),
+  );
+});
