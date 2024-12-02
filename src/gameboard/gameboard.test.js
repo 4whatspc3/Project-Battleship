84d58eb422ea52cd7d21c3e095b8d0a15bff806f
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2024-12-02 00:38:21 -0300

    fix: eliminate redundancies from gameboard tests

--- src/gameboard/gameboard.test.js
+++ src/gameboard/gameboard.test.js
@@ -1,31 +1,23 @@
 import gameboard from "./gameboard";
 
+const A = new gameboard();
+
 test("getBoard() returns a 2D Array with 10 arrays and 10 rows", () => {
-  const A = new gameboard();
   A.getBoard();
   expect(A.array2D.length === 10).toBe(true);
 });
 
 test("The three methods should return 'true'", () => {
-  const B = new gameboard();
+  A.array2D[5][6] = "missed shot";
+  A.array2D[3][2] = "there is a ship";
+  A.array2D[1][1] = "damaged ship";
 
-  B.getBoard();
-
-  B.array2D[5][6] = "missed shot";
-  B.array2D[3][2] = "there is a ship";
-  B.array2D[1][1] = "damaged ship";
-
-  expect(B.isMissing(5, 6)).toBe(true);
-  expect(B.isShip(3, 2)).toBe(true);
-  expect(B.isDamagedShip(1, 1)).toBe(true);
+  expect(A.isMissing(5, 6)).toBe(true);
+  expect(A.isShip(3, 2)).toBe(true);
+  expect(A.isDamagedShip(1, 1)).toBe(true);
 });
 
 test("isMissing() returns 'missed shot' if the given coordinates have their default value", () => {
-  const C = new gameboard();
-
-  C.getBoard();
-  C.array2D[3][2] = "there is a ship";
-
-  expect(C.isMissing(5, 6)).toBe(`missed shot`);
-  expect(C.isMissing(3, 2)).toBe(false);
+  expect(A.isMissing(8, 9)).toBe(`missed shot`);
+  expect(A.isMissing(3, 2)).toBe(false);
 });
