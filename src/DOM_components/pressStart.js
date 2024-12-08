723509375fe33bb2c48f2aa5518ee9d0716072fc
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2024-12-08 18:09:33 -0300

    feat: Announce that the game has started

--- src/DOM_components/pressStart.js
+++ src/DOM_components/pressStart.js
@@ -5,10 +5,14 @@ import strategia from "./strategia";
 const pressStart = () => {
   const start = document.querySelector(".start");
 
+  const infoGeneral = document.querySelector(".info-general p");
+
   start.addEventListener("click", () => {
     startGame();
 
     strategia();
+
+    infoGeneral.textContent = "The game has started";
 
     document.querySelector(".start").disabled = true;
   });
