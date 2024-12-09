ba384287ef201786b9bba97556c4a582f04ed1a3
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2024-12-09 20:25:32 -0300

    fix:  'startGame' function
    When invoked, the function creates the player 1
    and player 2.

--- src/DOM_components/startGame.js
+++ src/DOM_components/startGame.js
@@ -1,9 +1,14 @@
 import gameboard from "../gameboard/gameboard";
 
+import player from "../player/player";
+
 const startGame = () => {
   console.log("I'm working right now");
+  const player1 = new player("Human", "Player 1");
 
   const board1 = new gameboard();
+
+  const player2 = new player("Computer", "Player 2");
 
   const board2 = new gameboard();
 
