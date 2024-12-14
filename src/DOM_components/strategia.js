546074859a046833d2796e7fcf7c703c80f8f295
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2024-12-14 16:20:48 -0300

    fix: 'getShips' function
    Move getShips to its own module.

--- src/DOM_components/strategia.js
+++ src/DOM_components/strategia.js
@@ -1,4 +1,4 @@
-import ships from "../ships/ships";
+import getShips from "./getShips";
 
 import showInfoPlayer from "./showInfoPlayer";
 
@@ -14,7 +14,6 @@ const strategia = (playerBoard) => {
       const x = e.target.parentNode.dataset.x;
       const y = e.target.dataset.y;
 
-      console.log(`x: ${x}, y: ${y}`);
       getShips(turn, playerBoard, x, y);
 
       if (JSON.stringify(playerBoard) !== JSON.stringify(boardCopy)) {
@@ -26,40 +25,6 @@ const strategia = (playerBoard) => {
       }
     });
   });
-};
-
-const getShips = (counter, playerBoard, x, y) => {
-  const btn = document.querySelector(".board-1 button");
-
-  switch (counter) {
-    case 0:
-      const carrier = new ships(5, btn.className);
-      carrier.passCoordShip(playerBoard, x, y);
-      carrier.passCoordDOM(x, y);
-      break;
-    case 1:
-      const battleship = new ships(4, btn.className);
-      battleship.passCoordShip(playerBoard, x, y);
-      battleship.passCoordDOM(x, y);
-      break;
-    case 2:
-      const destroyer = new ships(3, btn.className);
-      destroyer.passCoordShip(playerBoard, x, y);
-      destroyer.passCoordDOM(x, y);
-      break;
-    case 3:
-      const submarine = new ships(3, btn.className);
-      submarine.passCoordShip(playerBoard, x, y);
-      submarine.passCoordDOM(x, y);
-      break;
-    case 4:
-      const patrolBoat = new ships(2, btn.className);
-      patrolBoat.passCoordShip(playerBoard, x, y);
-      patrolBoat.passCoordDOM(x, y);
-      break;
-    default:
-      console.log(counter);
-  }
 };
 
 export default strategia;
