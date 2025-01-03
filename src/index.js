a90ef9bd2a9572d6f05114f425e243a6847eba4d
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2025-01-03 18:03:14 -0300

    fix:  break 'style.css' file into smaller parts
    There are more files, but it's more readable
    and we can easily find the intended components

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-import "./style.css";
+import "./styles/style.css";
+
+import "./styles/style_info.css";
+
+import "./styles/style_gameboard.css";
+
+import "./styles/style_buttons.css";
+
+import "./styles/battleEffects.css";
 
 import pressStart from "./DOM_components/comp01_pressStart";
 
